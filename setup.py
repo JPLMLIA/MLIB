fd3a76a6d3176e195b89eb03aa296b59494339d3
Jack Lightholder <jack.a.lightholder@jpl.nasa.gov>
2020-04-20 13:33:59 -0700

    Updated setup, added gitignore

--- setup.py
+++ setup.py
@@ -1,14 +1,25 @@
 from setuptools import find_packages, setup
-
-with open ('requirements.txt') as f:
+with open('requirements.txt') as f:
     requirements = f.read().splitlines()
-
 if __name__ == '__main__':
     setup(
         name='mlib',
         version='1.0.0',
+        license='CalTech',
+        author='Lukas Mandrake',
+        author_email='lukas.mandrake@jpl.caltech.edu',
+        url='https://github.com/JPLMLIA/MLIB',
+        download_url='THIS IS A STUB',
         packages=find_packages(),
         include_package_data=True,
         package_data={},
-        install_requires=requirements
-    )
+        install_requires=requirements,
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Indended Audience :: Science/Research',
+            'Indended Audience :: Education',
+            'License :: CalTech License',
+            'Operating System :: POSIX :: Linux',
+            'Programming Language :: Python :: 3.6 :: Only',
+            'Topic :: Utilities'
+        ])
